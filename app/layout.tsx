0e2c905eb069256ec3ca51051e6454256ca57d98
Tiago Alves <alvesjtiago@gmail.com>
2025-05-02 00:11:47 -0400

    Coinaroid preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,33 @@ import type { Metadata } from 'next'
 import './globals.css'
 import Providers from '@/providers/Privy'
 
+const appUrl = 'https://coinaroid.xyz'
+
+const frame = {
+  version: 'next',
+  imageUrl: `${appUrl}/images/erica_coinaroid_iconURL_1024.png`,
+  button: {
+    title: 'Join Coinaroid',
+    action: {
+      type: 'launch_frame',
+      name: 'Coinaroid',
+      url: appUrl,
+      splashImageUrl: `${appUrl}/images/erica_splashImageUrl_200.png`,
+      splashBackgroundColor: '#6CCCBD',
+    },
+  },
+}
+
 export const metadata: Metadata = {
   title: 'Coinaroid',
-  description: 'Coinaroid',
+  description: 'Cast your coins',
+  openGraph: {
+    title: 'Coinaroid',
+    description: 'Cast your coins',
+  },
+  other: {
+    'fc:frame': JSON.stringify(frame),
+  },
 }
 
 export default function RootLayout({
