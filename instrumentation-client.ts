097901e227e6d7804c351358310ca5543ea21b61
Tiago Alves <alvesjtiago@gmail.com>
2025-05-05 09:24:32 -0400

    Add sentry

---
+++ instrumentation-client.ts
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://aae2f69e7b9cdbb03bb5ba58ba359cbf@o4509270755049472.ingest.us.sentry.io/4509270756163584",
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
